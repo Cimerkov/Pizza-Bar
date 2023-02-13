16983a2e50c341d78708bd0e6168bf4f22c4a447
Cimerkov <90925639+Cimerkov@users.noreply.github.com>
2023-02-13 14:12:17 +0100

    Home Section

---
+++ app.js
@@ -0,0 +1,32 @@
+let slideIndex = 1;
+showSlide(slideIndex);
+
+// change slide with the prev/next button
+function moveSlide(moveStep) {
+    showSlide(slideIndex += moveStep);
+}
+
+// change slide with the dots
+function currentSlide(n) {
+    showSlide(slideIndex = n);
+}
+
+function showSlide(n) {
+    let i;
+    const slides = document.getElementsByClassName("slide");
+   
+
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+
+    // hide all slides
+    for (i = 0; i < slides.length; i++) {
+        slides[i].classList.add('hidden');
+    }
+
+
+
+    // show the active slide
+    slides[slideIndex - 1].classList.remove('hidden');
+
+}
